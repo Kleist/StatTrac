56ef2f498aedb87eda4f7f9fb3d8d3de26aa6251
Andreas Kleist SVendsen <andreaskleist@gmail.com>
2012-09-10 22:28:19 +0200

    Add StatCounter to player and add EasyMock to be able to test it.

--- src/com/kleist/stattrac/Player.java
+++ src/com/kleist/stattrac/Player.java
@@ -3,10 +3,12 @@ package com.kleist.stattrac;
 public final class Player {
 	private String name_;
 	private int number_;
+	private StatCounter statCounter_;
 
 	public Player(String name, int number) {
 		name_ = name;
 		number_ = number;
+		statCounter_ = new StatCounter();
 	}
 
 	public String getName() {
@@ -15,5 +17,17 @@ public final class Player {
 	
 	public int getNumber() {
 		return number_;
+	}
+
+	public StatCounter getStatCounter() {
+		return statCounter_;
+	}
+
+	public void addEvent(StatEvent event) {
+		statCounter_.addEvent(event);
+	}
+
+	public void setStatCounter(StatCounter statCounter) {
+		statCounter_ = statCounter;
 	}
 }
--- test/com/kleist/stattrac/test/PlayerTest.java
+++ test/com/kleist/stattrac/test/PlayerTest.java
@@ -1,18 +1,46 @@
 package com.kleist.stattrac.test;
+
 import static org.junit.Assert.*;
 
+import org.easymock.EasyMock;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.kleist.stattrac.Player;
+import com.kleist.stattrac.StatCounter;
+import com.kleist.stattrac.StatEvent;
 
 public class PlayerTest {
-	@Test public void hasGivenName() {
-		Player p = new Player("Kleist", 7);
-		assertEquals("Kleist", p.getName());
+	private Player player;
+	private StatCounter statCounterMock;
+	@Before
+	public void setUp() {
+		player = new Player("Kleist", 7);
+		statCounterMock = EasyMock.createMock(StatCounter.class);
+		player.setStatCounter(statCounterMock);	
 	}
 
-	@Test public void hasGivenNumber() {
-		Player p = new Player("Kleist", 7);
-		assertEquals(7, p.getNumber());
+	@Test
+	public void hasGivenName() {
+		assertEquals("Kleist", player.getName());
+	}
+
+	@Test
+	public void hasGivenNumber() {
+		assertEquals(7, player.getNumber());
+	}
+
+	@Test
+	public void hasStatCounter() {
+		assertNotNull(player.getStatCounter());
+	}
+	
+	@Test
+	public void forwardsEventsToStatCounter() {
+		statCounterMock.addEvent(StatEvent.EXTRAPOINT);
+		EasyMock.expectLastCall();
+		EasyMock.replay(statCounterMock);
+		player.addEvent(StatEvent.EXTRAPOINT);
+		EasyMock.verify(statCounterMock);
 	}
 }
