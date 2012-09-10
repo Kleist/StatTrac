5e15ef3aff90cd1ef7288c3e00278418e65eaf69
Andreas Kleist SVendsen <andreaskleist@gmail.com>
2012-09-10 21:55:08 +0200

    Move points per event to StatEvent.

--- src/com/kleist/stattrac/PointCounter.java
+++ src/com/kleist/stattrac/PointCounter.java
@@ -1,32 +1,16 @@
 package com.kleist.stattrac;
 
-import java.util.EnumMap;
-
-
 public class PointCounter {
 	private StatCounter statCounter_;
-	private final EnumMap<StatEvent, Integer> pointsPerEvent_ = initPointsPerEvent();
 
 	public PointCounter(StatCounter sc) {
 		statCounter_ = sc;
 	}
 
-	private static EnumMap<StatEvent, Integer> initPointsPerEvent() {
-		EnumMap<StatEvent, Integer> pointMap = new EnumMap<StatEvent, Integer>(StatEvent.class);
-		for (StatEvent event : StatEvent.values()) {
-			pointMap.put(event, 0);
-		}
-		pointMap.put(StatEvent.EXTRAPOINT, 1);
-		pointMap.put(StatEvent.SAFETY, 2);
-		pointMap.put(StatEvent.TOUCHDOWN, 6);
-		pointMap.put(StatEvent.TWOPOINTCONVERSION, 2);
-		return pointMap;
-	}
-
 	public int getPoints() {
 		int points = 0;
 		for (StatEvent event : StatEvent.values()) {
-			points += pointsPerEvent_.get(event) * statCounter_.getEventCount(event);
+			points += event.getPoints() * statCounter_.getEventCount(event);
 		}
 		return points;
 	}
--- src/com/kleist/stattrac/StatEvent.java
+++ src/com/kleist/stattrac/StatEvent.java
@@ -1,5 +1,25 @@
 package com.kleist.stattrac;
 
+import java.util.EnumMap;
+
 public enum StatEvent {
-	INCOMPLETE, CATCH, FIRSTDOWN, TOUCHDOWN, EXTRAPOINT, TWOPOINTCONVERSION, SAFETY, SACK
+	INCOMPLETE, CATCH, FIRSTDOWN, TOUCHDOWN, EXTRAPOINT, TWOPOINTCONVERSION, SAFETY, SACK;
+
+	private static final EnumMap<StatEvent, Integer> pointsPerEvent_ = initPointsPerEvent();
+	
+	private static EnumMap<StatEvent, Integer> initPointsPerEvent() {
+		EnumMap<StatEvent, Integer> pointMap = new EnumMap<StatEvent, Integer>(StatEvent.class);
+		for (StatEvent event : StatEvent.values()) {
+			pointMap.put(event, 0);
+		}
+		pointMap.put(StatEvent.EXTRAPOINT, 1);
+		pointMap.put(StatEvent.SAFETY, 2);
+		pointMap.put(StatEvent.TOUCHDOWN, 6);
+		pointMap.put(StatEvent.TWOPOINTCONVERSION, 2);
+		return pointMap;
+	}
+	
+	public int getPoints() {
+		return pointsPerEvent_.get(this);
+	}
 }
