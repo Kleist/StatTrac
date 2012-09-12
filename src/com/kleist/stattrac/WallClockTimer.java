184459cc51655228b8801ad3f78cb1fcf795a335
Andreas Kleist Svendsen <andreaskleist@gmail.com>
2012-09-12 22:41:36 +0200

    Eliminate extra level of clocks

--- src/com/kleist/stattrac/WallClockTimer.java
+++ src/com/kleist/stattrac/WallClockTimer.java
@@ -1,29 +1,16 @@
 package com.kleist.stattrac;
 
 public class WallClockTimer {
-	public interface TimeSource {
-		public abstract long milliseconds();
-	}
-	private TimeSource timeSource_;
 	private long baseTime_;
 
 	public WallClockTimer() {
-		timeSource_ = new TimeSource() {
-			public long milliseconds() {
-				return System.currentTimeMillis();				
-			}
-		};
 	}
 	
 	public void resetClock() {
-		baseTime_ = timeSource_.milliseconds();
-	}
-
-	public void setTimeSource(TimeSource timeSource) {
-		timeSource_ = timeSource;
+		baseTime_ = System.currentTimeMillis();
 	}
 
 	public long getMilliSecondsSinceReset() {
-		return timeSource_.milliseconds() - baseTime_;
+		return System.currentTimeMillis() - baseTime_;
 	}
 }
