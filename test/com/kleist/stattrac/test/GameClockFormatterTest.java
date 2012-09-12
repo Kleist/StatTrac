5424a71c817f84efa3e51f72a9692d948a4ddfe8
Andreas Kleist Svendsen <andreaskleist@gmail.com>
2012-09-13 00:44:33 +0200

    Cleanup GameClockFormatterTest

--- test/com/kleist/stattrac/test/GameClockFormatterTest.java
+++ test/com/kleist/stattrac/test/GameClockFormatterTest.java
@@ -2,29 +2,17 @@ package com.kleist.stattrac.test;
 
 import static org.junit.Assert.assertEquals;
 
-import org.easymock.EasyMock;
 import org.junit.*;
 
-import com.kleist.stattrac.GameClock;
 import com.kleist.stattrac.GameClockFormatter;
-import com.kleist.stattrac.WallClockTimer;
 
 
 public class GameClockFormatterTest {
-	private GameClock gameClock;
 	private GameClockFormatter formatter;
-	private WallClockTimer clock;
 
 	@Before
 	public void setUp() {
-		clock = EasyMock.createMock(WallClockTimer.class);
-		gameClock = new GameClock();
-		gameClock.setClock(clock);
 		formatter = new GameClockFormatter();
-	}
-
-	public GameClockFormatterTest(long millisLeft) {
-		gameClock = new GameClock();
 	}
 
 	@Test
